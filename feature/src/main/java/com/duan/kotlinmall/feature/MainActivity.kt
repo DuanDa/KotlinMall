d5bd933f278e188012d8907fcf383ea05b4e3133
Duan <dansuninho@gmail.com>
2018-08-19 21:57:25 +0800

    first commit

---
+++ feature/src/main/java/com/duan/kotlinmall/feature/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.duan.kotlinmall.feature
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.duan.kotlinmall.feature.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
